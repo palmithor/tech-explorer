744c097e2c1a8d992666e4b5451b574c972eb94c
Pálmi Þór Valgeirsson <palmiv@tempo.io>
2024-08-23 07:44:55 +0000

    tanstack-start

---
+++ packages/tanstack-start/app/client.tsx
@@ -0,0 +1,8 @@
+import { hydrateRoot } from "react-dom/client";
+import { StartClient } from "@tanstack/start";
+import { createRouter } from "./router";
+
+const router = createRouter();
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+hydrateRoot(document.getElementById("root")!, <StartClient router={router} />);
